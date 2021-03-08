33fc7d3c2a42f889ee9dcf25d609b43d9ebc7327
miketheologitis <miketh@outlook.com.gr>
2021-03-09 01:33:31 +0200

    Nous1

--- askisi1/askisi1/__main__.py
+++ askisi1/askisi1/__main__.py
@@ -1,4 +1,6 @@
 import sys
+from pathlib import Path
+from initializeProblem import*
 
 def main(args=None):
 
@@ -6,7 +8,19 @@ def main(args=None):
     if args is None:
         args = sys.argv[1:]
 
-    print("Hello world")
+    #https://stackoverflow.com/questions/40416072/reading-file-using-relative-path-in-python-project
+    path = Path(__file__).parent  / "../data/myTest1.txt"
+    file = open(path, "r")
+
+    source = getSource(file)
+    destination = getDestination(file)
+
+    #https://codereview.stackexchange.com/questions/163414/adjacency-list-graph-representation-on-python
+
+    getGraphAndWeights(file)
+    file.close()
+
+
 
 if __name__ == "__main__":
     sys.exit(main())
